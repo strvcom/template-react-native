d4aae14b9d2e2da3c5dcc94c1297ad22e923955d
Peter Varholak <peter.varholak@gmail.com>
2019-08-17 19:39:48 +0200

    setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,38 @@
+'use strict'
+
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
-};
+  env: {
+    browser: true,
+    jest: true,
+  },
+  extends: [
+    '@strv/react-native',
+    '@strv/react-native/optional',
+    '@strv/react-native/style',
+    '@strv/eslint-config-typescript',
+    '@strv/eslint-config-typescript/optional',
+    '@strv/eslint-config-typescript/style',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['prettier'],
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'comma-dangle': 'off',
+    'function-paren-newline': 'off',
+    'id-length': 'off',
+    'import/exports-last': 'off',
+    'import/group-exports': 'off',
+    'import/namespace': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'import/no-unresolved': 'off',
+    'no-extra-parens': 'off',
+    'no-shadow': 'off',
+    'operator-linebreak': 'off',
+    'padding-line-between-statements': 'off',
+    'react/prop-types': 'off',
+    'react/state-in-constructor': 'off',
+    'react-native/no-raw-text': 'off',
+  },
+}
--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
-  preset: 'react-native',
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.jest.json',
+    },
+  },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  preset: 'react-native',
+  testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
+  transform: {
+    '^.+\\.js$': '<rootDir>/node_modules/react-native/jest/preprocessor.js',
+    '\\.(ts|tsx)$': 'ts-jest',
+  },
 }
