a2344c73667713c61512014657654b8b5e803cd7
Peter Varholak <peter.varholak@gmail.com>
2019-10-19 19:41:14 +0200

    feat: add CI setup

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,24 +1,17 @@
 import envVars, { NativeConfig } from 'react-native-config'
 
-enum requiredVariables {
-  'FOO',
-}
+const requiredVariables = ['FOO'] as const
 
-type RequiredVariables = keyof typeof requiredVariables
-type Config = Readonly<{ [key in RequiredVariables]: string }>
+type RequiredVariables = Omit<typeof requiredVariables, keyof []>
+type Config = { [key in RequiredVariables[keyof RequiredVariables]]: string }
 
-const ensureConfig = (env: NativeConfig): Config =>
-  Object.values(requiredVariables).reduce((acc, val) => {
-    // Filters index entries from enum.
-    if (typeof val === 'number') {
-      return acc
-    }
-
+const ensureConfig = (env: NativeConfig) =>
+  requiredVariables.reduce((acc, val) => {
     if (!env[val]) {
       throw new Error(`Missing '${val}' environment variable.`)
     }
 
     return { ...acc, [val]: env[val] }
-  }, {})
+  }, {}) as Config
 
 export default ensureConfig(envVars)
